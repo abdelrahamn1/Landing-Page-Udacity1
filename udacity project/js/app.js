55304fe1cd111b8efe940f6d0caa8705fb6affea
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2022-10-20 23:41:27 +0200

    Update app.js

--- udacity project/js/app.js
+++ udacity project/js/app.js
@@ -1,142 +1,99 @@
 /**
- *
+ * 
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- *
+ * 
  * Dependencies: None
- *
+ * 
  * JS Version: ES2015/ES6
- *
+ * 
  * JS Standard: ESlint
- *
- */
+ * 
+*/
 
 /**
  * Define Global Variables
- *
- */
-const mybutton = document.getElementById('myBtn');
-const sectionsElements = document.querySelectorAll('section');
-const navbarUl = document.getElementById('navbar__list');
-// change title's text
-const title = document.getElementById('landing-title');
+ * 
+*/
+const fragment = document.createDocumentFragment();
+const sectionList = document.querySelectorAll('section');
 
-let navList = '';
-title.textContent = `Udactiy's Project`;
 /**
  * End Global Variables
  * Start Helper Functions
- 
- *
- */
-// Generate navbar from sections id names we got from the querySelectorAll
-function gernerateNavbar() {
-  sectionsElements.forEach((section) => {
-    // add html tags for list items
-    // dataset.nav returns DOMStringMap {nav: section 1}
-    navList += `<li> <a class="nav__link menu__link" href="#${section.id}" id="navli">
-          ${section.dataset.nav}</a></li>`;
-  });
-  // add the tags to the inner htmls
-  navbarUl.innerHTML = navList;
+ * 
+*/
+function createNavItemHTML(id, name){
+    const itemHTML = `<a class ="menu__link" data-id="${id}">${name}</a>`;
+    return itemHTML;
 }
-gernerateNavbar();
-
-// Add class 'active' to section when near top of viewport (Eye level )
-
-function addActiveClass(section) {
-  // get the id from the section
-  const id = section.getAttribute('id');
-
-  // add the active class to the section
-  document.querySelector(`#${id}`).classList.add('your-active-class');
-}
-
-//Removing the active class from the section
-function removeActiveClass(section) {
-  const id = section.getAttribute('id');
-  document.querySelector(`#${id}`).classList.remove('your-active-class');
-}
-
-// calcualting when the section is active
-function makeActiveSection() {
-  sectionsElements.forEach((section) => {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
-    // DOMRect object providing information about the size of an element and its position relative to the viewport.
-    // viewport : A viewport represents a polygonal (normally rectangular) area in computer graphics that is currently being viewed.
-
-    let elementOffset = section.getBoundingClientRect();
-    if (elementOffset.top <= 150 && elementOffset.bottom >= 150) {
-      addActiveClass(section);
-    } else {
-      removeActiveClass(section);
-    }
-  });
-}
-// event listener to the dom itself so
-document.addEventListener('scroll', makeActiveSection);
-
-// get a button to scroll back to top of the page
-//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
+function isInViewport (elem) {
+    const bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = 'block';
-  } else {
-    mybutton.style.display = 'none';
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
-mybutton.addEventListener('click', topFunction);
-
-let navbar = document.getElementById('navbar').querySelectorAll('li');
-
-// itrate in li items list
-navbar.forEach((item) => {
-  item.addEventListener('click', function (e) {
-    navbar.forEach((item) => {
-      // remove every navbarclick class added befoe in any list item
-      item.classList.remove('navbarclick');
-    });
-    // add the class on the button
-    item.classList.add('navbarclick');
-  });
-});
-
-// lis.forEach((li) => {
-//   li.addEventListener('mouseover', function (e) {
-//     li.classList.add('navbarclick');
-//     console.log(li.innerHTML);
-//   });
-// });
 /**
  * End Helper Functions
  * Begin Main Functions
- *
- */
+ * 
+*/
 
 // build the nav
+function buildNavigation(){
+    for (let i=0; i < sectionList.length; i++){
+        const newMenuItem = document.createElement('li');
+        const sectionName = sectionList[i].getAttribute('data-nav')
+        const sectionId = sectionList[i].getAttribute('id')
+        newMenuItem.innerHTML = createNavItemHTML(sectionId, sectionName)
+         fragment.appendChild(newMenuItem);
+    }
+    const navBarList = document.getElementById('navbar__list')
+    navBarList.appendChild(fragment);
+}
 
 // Add class 'active' to section when near top of viewport
+function setActiveClass(){
+    for (let i=0; i < sectionList.length; i++){
+        if (isInViewport(sectionList[i])){
+            sectionList[i].classList.add("your-active-class");
+        }else{
+            sectionList[i].classList.remove("your-active-class");
+        }
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
+function scrollToElement(event){
+    if(event.target.nodeName === 'A'){
+        const sectionId = event.target.getAttribute('data-id');
+        const section = document.getElementById(sectionId);
+        section.scrollIntoView({behavior: "smooth"});
+    }
+}
+
+
 
 /**
  * End Main Functions
  * Begin Events
- *
- */
+ * 
+*/
+document.addEventListener('scroll', function(){
+    setActiveClass();
+});
+const navBarList = document.getElementById('navbar__list')
+navBarList.addEventListener('click', function(event){
+    scrollToElement(event)
+})
+// Build menu 
+buildNavigation()
+// Scroll to section on link click
 
-// Build menu
+// Set sections as active
