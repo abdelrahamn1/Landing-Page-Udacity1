883347014f74b49da7089f7bdc7c6916bc2b3b14
Abdelrahman <86893144+abdelrahamn1@users.noreply.github.com>
2022-10-20 23:31:20 +0200

    Update app.js

--- udacity project/js/app.js
+++ udacity project/js/app.js
@@ -13,112 +13,130 @@
  *
  */
 
-/*
- * This code was written, tested, debugged, and operated by me only Muhammad Nasif, all rights reserved only to me and Udacity.
- * Everything done in the code has its own comment above it.
+/**
+ * Define Global Variables
+ *
  */
+const mybutton = document.getElementById('myBtn');
+const sectionsElements = document.querySelectorAll('section');
+const navbarUl = document.getElementById('navbar__list');
+// change title's text
+const title = document.getElementById('landing-title');
 
-// Start Global Variables
-const navBar = document.querySelector(".navbar__menu");
-const navList = document.querySelector("#navbar__list");
-const sections = document.querySelectorAll("section");
-const footer = document.querySelector("footer");
-const header = document.querySelector(".page__header");
-// End Global Variables
-
-// Start build the nav
-function buildNav() {
-  sections.forEach((section) => {
-    //Create the li elements that contained inside the ul
-    const navButton = document.createElement("li");
-    //Insert the html text to  the li
-    navButton.insertAdjacentHTML(
-      "afterbegin",
-      `<a href="#${section.id}" class="menu__link">${section.dataset.nav}</a>`
-    );
-    //Append the li to the ul
-    navList.appendChild(navButton);
-
-    //scrollBehavior Function Invoke
-    scrollBehavior(navButton, section);
+let navList = '';
+title.textContent = `Udactiy's Project`;
+/**
+ * End Global Variables
+ * Start Helper Functions
+ 
+ *
+ */
+// Generate navbar from sections id names we got from the querySelectorAll
+function gernerateNavbar() {
+  sectionsElements.forEach((section) => {
+    // add html tags for list items
+    // dataset.nav returns DOMStringMap {nav: section 1}
+    navList += `<li> <a class="nav__link menu__link" href="#${section.id}" id="navli">
+          ${section.dataset.nav}</a></li>`;
   });
-  //Append the ul to the nav
-  navBar.appendChild(navList);
+  // add the tags to the inner htmls
+  navbarUl.innerHTML = navList;
+}
+gernerateNavbar();
+
+// Add class 'active' to section when near top of viewport (Eye level )
+
+function addActiveClass(section) {
+  // get the id from the section
+  const id = section.getAttribute('id');
+
+  // add the active class to the section
+  document.querySelector(`#${id}`).classList.add('your-active-class');
 }
 
-//Build Nav Function Invoke
-buildNav();
-
-//End build the nav
-
-// Start of Scroll to anchor ID using scrollTO event
-function scrollBehavior(navButton, section) {
-  navButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    window.scrollTo({
-      top: section.offsetTop,
-      behavior: "smooth",
-    });
-  });
+//Removing the active class from the section
+function removeActiveClass(section) {
+  const id = section.getAttribute('id');
+  document.querySelector(`#${id}`).classList.remove('your-active-class');
 }
-// End of Scroll to anchor ID using scrollTO event
 
-// Start of Set the Section class 'active' when it near to the top of viewport
-function activeSection() {
-  // Select all anchor using "menu__link" class
-  const navActive = document.querySelectorAll(".menu__link");
-  sections.forEach((section, i) => {
-    //Get the boundingrect for each section
-    const sectionBond = section.getBoundingClientRect();
-    //Check if the section is in viewport or not
-    if (sectionBond.top <= 380 && sectionBond.bottom >= 350) {
-      //section in viewport accourding to top and bottom boundings
-      //add 'your-active-class' class to the specific section
-      section.classList.add("your-active-class");
-      //add 'active_button' class to the specific nav button according to section ID
-      navActive[i].classList.add("active_button");
+// calcualting when the section is active
+function makeActiveSection() {
+  sectionsElements.forEach((section) => {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+    // DOMRect object providing information about the size of an element and its position relative to the viewport.
+    // viewport : A viewport represents a polygonal (normally rectangular) area in computer graphics that is currently being viewed.
+
+    let elementOffset = section.getBoundingClientRect();
+    if (elementOffset.top <= 150 && elementOffset.bottom >= 150) {
+      addActiveClass(section);
     } else {
-      //Remove both section and navButton active classes when section is off sight
-      section.classList.remove("your-active-class");
-      navActive[i].classList.remove("active_button");
+      removeActiveClass(section);
     }
   });
 }
-// End of Set the Section class 'active' when it near to the top of viewport
+// event listener to the dom itself so
+document.addEventListener('scroll', makeActiveSection);
 
-// Start of Toggle the NavBar According to User Scroll Activity
-function toggleNavBar() {
-  let userScroll;
-  //Default Settings for NavBar while scrolling
-  header.style.cssText = "opacity: 1; transition: ease 0.3s ;";
-  // Cleartimeout throughout the scrolling
-  window.clearTimeout(userScroll);
-  //The Timeout to run after scrolling ends
-  userScroll = setTimeout(function () {
-    //The Settings Executed on NavBar after Timeout finished
-    header.style.cssText = "opacity: 0; transition: ease 0.3s ;";
-  }, 6000);
+// get a button to scroll back to top of the page
+//https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
 }
-// End of Toggle the NavBar According to User Scroll Activity
 
-//Start of the Scroll Event to execute the functions of activeSection and toggleNavBar
-window.addEventListener("scroll", (event) => {
-  activeSection();
-  toggleNavBar();
-});
-//End of the Scroll Event to execute the functions of activeSection and toggleNavBar
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
-// Start of GO UP Button
-//Create the div element for the button
-const goUpButton = footer.insertAdjacentHTML(
-  "beforebegin",
-  `<div Id="return_top" ></div>`
-);
-// Scroll to top of the Landing Page using scrollTO event
-document.getElementById("return_top").addEventListener("click", function () {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
+mybutton.addEventListener('click', topFunction);
+
+let navbar = document.getElementById('navbar').querySelectorAll('li');
+
+// itrate in li items list
+navbar.forEach((item) => {
+  item.addEventListener('click', function (e) {
+    navbar.forEach((item) => {
+      // remove every navbarclick class added befoe in any list item
+      item.classList.remove('navbarclick');
+    });
+    // add the class on the button
+    item.classList.add('navbarclick');
   });
 });
-// End of GO UP Button
+
+// lis.forEach((li) => {
+//   li.addEventListener('mouseover', function (e) {
+//     li.classList.add('navbarclick');
+//     console.log(li.innerHTML);
+//   });
+// });
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+// Add class 'active' to section when near top of viewport
+
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// Build menu
